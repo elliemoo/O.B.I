e6465597b9599a237d1e33171c1ee8b413d41bca
Elliott Yu <elliottyu@Elliotts-Macbook-Pro-13.local>
2022-04-03 22:10:11 +1000

    updating frontend

--- o.b.i_studio/src/components/Logo.jsx
+++ o.b.i_studio/src/components/Logo.jsx
@@ -1,15 +1,10 @@
 import OBI_logo from "../Images/OBI_logo.gif";
-import Button from 'react-bootstrap/Button'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 const Logo = () => {
     return(
-        
-        <div>
-            <img className="App-logo"src={OBI_logo} alt="O.B.I Logo"/>
-            <Button>Test Button</Button>
-        </div>
-        
+        <img className="App-logo"src={OBI_logo} alt="O.B.I Logo"/>
     )
 }
 
