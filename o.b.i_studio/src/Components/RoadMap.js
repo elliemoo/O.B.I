e4976c83db9268a6606c1bd25c571968cc8e711f
Chowdary1999 <51407838+Chowdary1999@users.noreply.github.com>
2022-04-08 16:39:59 +1000

    Updated Roadmap

--- o.b.i_studio/src/Components/RoadMap.js
+++ o.b.i_studio/src/Components/RoadMap.js
@@ -5,28 +5,25 @@ const RoadMap = () => {
     <Card id='RoadMap' fluid >
       <Card.Header><h2>Our RoadMap</h2></Card.Header>
       <Card.Body>
-        <Card.Title><h2>Phase 1️⃣</h2></Card.Title>
+        <Card.Title><h2>Pre-Mint</h2></Card.Title>
         <Card.Text>
-          <h4>✨ First Month's Plans: April 2022 🗓️</h4> <br></br>
-          💻 Full Art Presentation/Reveal of Zoth(Will be Recorded)💻<br></br>
+          <h4>✨Phase 1️⃣ 🗓️</h4> <br></br>
           📝 Birth of Zoth Lore/Sin's Taking Over Lore📝<br></br>
-          Website Drop & Utility Explanation<br></br>
-          🪙  Crypto Giveaway ETH, SHIB, Sol 🪙 <br></br>
+          Utility Explanation about the staking and webnovel<br></br>
           🗣️ WL/OG Teaching Sessions(Crypto/NFT Advice & Other Trades) 🗣️ <br></br>
-          Mental Health Cast Daily & Streams <br></br>
-          🎧 Soundtrack Snippet Discord Listening Party🎧<br></br>
+          Mental Health Cast <br></br>
+          🎧 Soundtrack Discord Listening Party🎧<br></br>
           🧑‍💼 Core Team AMA/Full Team Introduction🧑‍💼<br></br>
 
-          <h4>✨ Second Month's Plans: May 2022:🗓️</h4><br></br>
+          <h4>✨✨Phase 2️⃣🗓️</h4><br></br>
 
           📖 O.B.I. Studios Book Club 📖 <br></br>
           💝 The Good Deed Challenge Introduction💝 <br></br>
           🧐 Artist Presentation & Full Website Update 🧐 <br></br>
           📦 First Collection Drop📦 <br></br>
           👨‍💼 First Collection Informational Session👨‍💼<br></br>
-                          ⏫ Don’t miss it!⏫ 
 
-          <h4>✨ Third Month's Plans: June 2022🗓️</h4><br></br>
+          <h4>✨✨✨Phase 3️⃣🗓️</h4><br></br>
 
           🧙 NFT Web Novel's made for each Race 🧙 <br></br>
           🌏 Merch Deals: Figurine Collection, Published Books, and Apparel 🌏 <br></br>
@@ -34,20 +31,19 @@ const RoadMap = () => {
                        💱 ⏫ Don’t miss it! ⏫ 💱
 
         </Card.Text>
-        <Card.Title><h2>🎆 Phase 2️⃣  Reveal Event🎆 </h2></Card.Title>
+        <Card.Title><h2>Post-Mint </h2></Card.Title>
         <Card.Text>
            🌋 Hellion's Gate Opening: [Game Development Period]🌋
 
-          <h4>✨ Fourth Month's Plans: July 2022🗓️</h4><br></br>
+          <h4>✨✨✨✨Phase 4️⃣🗓️</h4><br></br>
 
           🤯 Announcing Population 2 Collection!(Race Fusion) 🤯<br></br>
           🏃‍♂️  Animation Reveal/Presentation's🏃‍♂️ <br></br>
           🔥  Semi-Full Soundtrack Reveal!🔥 <br></br>
           🗞️ Monthly Development Presentation🗞️ <br></br>
+          🛬 Airdrop Educational Session 🛬<br></br>
 
-              🛬 Airdrop Educational Session 🛬<br></br>
-
-          <h4>✨ Fifth Month's Plans: August 2022🗓️</h4><br></br>
+          <h4>✨✨✨✨✨Phase 5️⃣🗓️</h4><br></br>
 
            🙌 Exclusive Airdrops of Exclusive Holder Items🙌<br></br>
            📦Holder Exclusive Airdrop Month Event Announcement📦 <br></br>
@@ -55,7 +51,7 @@ const RoadMap = () => {
            👀 Live Exclusive Holder Item Creation Presentation👀<br></br>
                 🔐 Exclusive Development Update🔐
 
-          <h4>✨ Sixth Month's Plans: September 2022🗓️</h4><br></br>
+          <h4>✨✨✨✨✨✨Phase 6️⃣🗓️</h4><br></br>
 
             <h3>To be continued.......🪐</h3>
         </Card.Text>
