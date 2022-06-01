c4263b4f9192690557bc5d6adea266f025a0a2ee
Chowdary1999 <51407838+Chowdary1999@users.noreply.github.com>
2022-06-01 14:47:49 +1000

    Added vue package for LuckyBox usage

--- o.b.i_studio/src/Components/LuckyBox.js
+++ o.b.i_studio/src/Components/LuckyBox.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useEffect, useState } from "react";
+import Vue from 'vue'
+import { VuePlugin } from 'vuera'
+Vue.use(VuePlugin)
 
 const LuckyBox = () => {
     return(
